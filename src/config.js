e39c625139f8d548f041324aa73124f009fe75bc
SaiKyauk <80118792+nicholas-kham@users.noreply.github.com>
2021-03-29 12:36:07 +0630

    added analytic

--- src/config.js
+++ src/config.js
@@ -10,6 +10,7 @@ export const config = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
